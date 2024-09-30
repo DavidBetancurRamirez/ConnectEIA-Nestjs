7e0a4029ffd584e9aafcf837321f193d4f6e08b9
David Betancur <david.betancur.ramirez.dbr@gmail.com>
2024-09-30 17:41:12 -0500

    Pasando el metodo generateTokens al token.service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,7 +3,6 @@ import { UserService } from '../user/user.service';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
 import { LoginResponse } from './interfaces/auth.interfaces';
-import { UserResponse } from '../common/interfaces/user.interface';
 import { TokenService } from './token.service';
 
 @Injectable()
@@ -15,12 +14,12 @@ export class AuthService {
 
   async register(registerDto: RegisterDto): Promise<LoginResponse> {
     const user = await this.userService.create(registerDto);
-    return this.generateTokens(user);
+    return this.tokenService.generateTokens(user);
   }
 
   async login(loginDto: LoginDto): Promise<LoginResponse> {
     const user = await this.userService.validateUser(loginDto.email, loginDto.password);
-    return this.generateTokens(user);
+    return this.tokenService.generateTokens(user);
   }
 
   async refreshToken(refreshToken: string): Promise<LoginResponse> {
@@ -29,12 +28,6 @@ export class AuthService {
     if (!user) {
       throw new BadRequestException('Invalid or expired access token');
     }
-    return this.generateTokens(user);
-  }
-
-  async generateTokens(user: UserResponse): Promise<LoginResponse> {
-    const accesToken = this.tokenService.generateAccessToken(user);
-    const refreshToken = this.tokenService.generateRefreshToken(user);
-    return { accesToken, refreshToken, data: user };
+    return this.tokenService.generateTokens(user);
   }
 }
